0ab52e6dbfb61d986dc960e3292a3ccb96f3e478
Brainybrian316 <brainybrian316@gmail.com>
2022-08-06 12:47:04 -0400

    filter terniary operator

--- react-app/src/components/Expenses/Expenses.js
+++ react-app/src/components/Expenses/Expenses.js
@@ -5,31 +5,30 @@ import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 
 
-const  Expenses = (props) => {
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
-const filterChangeHandler = selectedYear => {
-  setFilteredYear(selectedYear);
-};
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
-const filteredExpenses = props.items.filter(expense => {
-  return expense.date.getFullYear().toString() === filteredYear;
-});
-  
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
-    <Card className="expenses">
-    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}  />
-    {filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        />
-    ))}
-  
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {filteredExpenses.length === 0 ? <p> No expenses found.</p> : filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </div>
   );
 }
