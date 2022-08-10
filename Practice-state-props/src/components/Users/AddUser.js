cc9ce648ac0b8176013650fcfbd4799912637467
Brainybrian316 <brainybrian316@gmail.com>
2022-08-09 22:55:27 -0400

    removing state for refs on input form

--- Practice-state-props/src/components/Users/AddUser.js
+++ Practice-state-props/src/components/Users/AddUser.js
@@ -6,32 +6,32 @@ import classes from './AddUser.module.css';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
   const nameInputRef = useRef();
   const ageInputRef = useRef();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    const 
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).'
       })
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age greater than 0.'
       })
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredName, enteredUserAge);
+  
   };
 
   const usernameChangeHandler = (event) => {
@@ -55,16 +55,12 @@ const AddUser = (props) => {
         <input
           id="username"
           type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
           ref={nameInputRef}
         />
         <label htmlFor="age">Age (Years)</label>
         <input
           id="age"
           type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
           ref={ageInputRef}
         />
         <Button type="submit">Add User</Button>
