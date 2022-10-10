5107d9488b46e355d2edceacae01d84a3f7a448f
Brainybrian316 <brainybrian316@gmail.com>
2022-10-10 15:02:43 -0400

    context provider consumer hook approach

--- advanced-handling/src/components/MainHeader/Navigation.js
+++ advanced-handling/src/components/MainHeader/Navigation.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+	// return (
+	// <AuthContext.Consumer>
+	// 	{(context) => {
+
+	const context = useContext(AuthContext);
 	return (
-		<AuthContext.Consumer>
-			{(ctx) => {
-				return (
-					<nav className={classes.nav}>
-						<ul>
-							{ctx.isLoggedIn && (
-								<li>
-									<a href="/">Users</a>
-								</li>
-							)}
-							{props.isLoggedIn && (
-								<li>
-									<a href="/">Admin</a>
-								</li>
-							)}
-							{props.isLoggedIn && (
-								<li>
-									<button onClick={props.onLogout}>Logout</button>
-								</li>
-							)}
-						</ul>
-					</nav>
-				);
-			}}
-		</AuthContext.Consumer>
+		<nav className={classes.nav}>
+			<ul>
+				{context.isLoggedIn && (
+					<li>
+						<a href="/">Users</a>
+					</li>
+				)}
+				{context.isLoggedIn && (
+					<li>
+						<a href="/">Admin</a>
+					</li>
+				)}
+				{context.isLoggedIn && (
+					<li>
+						<button onClick={props.onLogout}>Logout</button>
+					</li>
+				)}
+			</ul>
+		</nav>
 	);
+	// }}
+	// </AuthContext.Consumer>
+	// );
 };
 
 export default Navigation;
